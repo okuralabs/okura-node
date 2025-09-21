23c87371e2a64a25887978c2108fc9220a45aaf3
wonabru <wonabru@gmail.com>
2025-09-21 18:30:47 +0200

    purge Channels

--- services/nonceService/serviceNonce.go
+++ services/nonceService/serviceNonce.go
@@ -272,7 +272,7 @@ func sendNonceMsg(ip [4]byte, topic [2]byte) {
 func Send(addr [4]byte, nb []byte) bool {
 	nb = append(addr[:], nb...)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
 	defer cancel()
 
 	// Try in a goroutine with timeout
