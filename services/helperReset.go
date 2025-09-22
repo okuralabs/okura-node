f2ab33859d69dfa8026e188b0ea4ac86e8cc6c92
wonabru <wonabru@gmail.com>
2025-09-22 12:42:33 +0200

    purge Channels

--- services/helperReset.go
+++ services/helperReset.go
@@ -20,7 +20,6 @@ func PurgeChannel(ch chan []byte, count int) {
 			// Discard the message
 		default:
 			// Channel is empty, done purging
-			logger.GetLogger().Panicln("channel purged")
 			return
 		}
 	}
