c4326c4f5ae6369da94a1e1083010cfcd9f4c6cc
wonabru <wonabru@gmail.com>
2025-12-31 20:41:02 +0100

    transactions broadcasting

--- services/transactionServices/serviceTransaction.go
+++ services/transactionServices/serviceTransaction.go
@@ -161,7 +161,7 @@ func BroadcastTxn(ignoreAddr [4]byte, nb []byte) {
 	num_peers := len(peers)
 	for topicip, _ := range peers {
 		// trying to send randomly to 1 other nodes
-		if rand.Intn(num_peers) >= 1 {
+		if !bytes.Equal(ignoreAddr[:], []byte{0, 0, 0, 0}) || rand.Intn(num_peers) >= 1 {
 			continue
 		}
 		copy(ip[:], topicip[2:])
