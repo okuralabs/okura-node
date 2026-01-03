da1232fb462b617139d2f2d85250d89793bd83ff
wonabru <wonabru@gmail.com>
2026-01-03 18:36:30 +0100

    blockMutex added

--- common/variables.go
+++ common/variables.go
@@ -10,7 +10,7 @@ var height int64
 var heightMax int64
 var heightMutex sync.RWMutex
 
-// var BlockMutex sync.Mutex
+var BlockMutex sync.Mutex
 // var NonceMutex sync.Mutex
 var IsSyncing = atomic.Bool{}
 var IsMiner = atomic.Bool{}
